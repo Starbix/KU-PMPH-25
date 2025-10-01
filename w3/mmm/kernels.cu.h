062510ab87bcfbc0c2825abc2f1b54596f5f25d9
Cédric Laubacher <cedric@laubacher.io>
2025-10-01 10:39:54 +0200

    Clean up whitespace

--- w3/mmm/kernels.cu.h
+++ w3/mmm/kernels.cu.h
@@ -2,12 +2,12 @@
 #define MULT_KERNELS
 
 // widthA = heightB
-template <class ElTp> 
+template <class ElTp>
 __global__ void mmmNaiveKer(ElTp* A, ElTp* B, ElTp* C, int heightA, int widthB, int widthA) {
   ElTp accum = 0.0f;
 
   int gidx = blockIdx.x*blockDim.x + threadIdx.x;
-  int gidy = blockIdx.y*blockDim.y + threadIdx.y; 
+  int gidy = blockIdx.y*blockDim.y + threadIdx.y;
 
   if( (gidx >= widthB) || (gidy >= heightA) ) return;
 
@@ -36,7 +36,7 @@ __global__ void mmmSymBlkRegInnSeqKer(ElTp* A, ElTp* B, ElTp* C, int heightA, in
   __shared__ ElTp Aloc[Ty*Ry][Tk];
 
   // remapping (a slice of) B to shared memory
-  __shared__ ElTp Bloc[Tk][Tx*Rx]; 
+  __shared__ ElTp Bloc[Tk][Tx*Rx];
 
   // the thread result is computed in register memory
   // and the global-memory array C is updated at the end.
@@ -46,7 +46,7 @@ __global__ void mmmSymBlkRegInnSeqKer(ElTp* A, ElTp* B, ElTp* C, int heightA, in
 
   unsigned int iii = blockIdx.y * Ty * Ry;
   unsigned int jjj = blockIdx.x * Tx * Rx;
-  
+
   unsigned int i = iii + tidy;
   unsigned int j = jjj + tidx;
 
@@ -63,28 +63,28 @@ __global__ void mmmSymBlkRegInnSeqKer(ElTp* A, ElTp* B, ElTp* C, int heightA, in
       /***************************************
        * Cuda Exercise 3:
        * Task 3.1:
-       *   Insert here the Cuda code that collectively copies---with 
+       *   Insert here the Cuda code that collectively copies---with
        *     all the T x T threads of the Cuda block---the
        *     corresponding slice from A and B  (hold in global memory)
        *     into shared-memory arrays Aloc[T*R][T] and Bloc[T][T*R].
        *   You may assume (everywhere, including the explanation below)
        *     that T = Tx = Ty = Tk AND R = Rx = Ry,
-       *     i.e., Tx, Ty, Tk have the same value and 
+       *     i.e., Tx, Ty, Tk have the same value and
        *         Rx and Ry have the same value.
        ***************************************************************/
 
       /***************************************
        * Subtask 3.1.1:
-       * Please insert here the code that collectively copies 
-       *    with the Ty x Tx threads of the Cuda block the 
-       *    following slice of A: 
-       *         A[iii : iii + Ty*Ry][kk : kk+Tk] 
-       *    to the shared-memory buffer Aloc, such that all accesses 
+       * Please insert here the code that collectively copies
+       *    with the Ty x Tx threads of the Cuda block the
+       *    following slice of A:
+       *         A[iii : iii + Ty*Ry][kk : kk+Tk]
+       *    to the shared-memory buffer Aloc, such that all accesses
        *    to A (and Aloc) are coalesced, i.e., consecutive
        *    threads on the threadIdx.x dimension having the same
        *    threadIdx.y value read consecutive locations in memory!
        * If the index is out of the bounds of A then write 0 in Aloc.
-       * 
+       *
        * Hints:
        * 1. Remember that threadIdx.y = 0 ... Ty-1 and, for simplicity
        *      assume that Tx = Tk, hence threadIdx.x = 0 ... Tk-1
@@ -96,29 +96,29 @@ __global__ void mmmSymBlkRegInnSeqKer(ElTp* A, ElTp* B, ElTp* C, int heightA, in
        *      heightA and the column index is less than widthA
        * 3. Remember that A is a flat one-dimensional array, hence you
        *      need to flatten the indexing, but
-       *      Aloc is a two-dimensional array 
+       *      Aloc is a two-dimensional array
        *      (see definitiona above at the beginning of kernel).
        * 4. Coalesced access to A essentially means that indexing in A
        *      should expand to something like: A[ ... + threadIdx.x]
        **************************************************************/
       for (int r = 0; r < Ry; r++) {
           Aloc[tidy][tidx] = (r < heightA && kk + tidx < widthA) ?
-                             A[r*widthA + kk + tidx] : 0.0; 
+                             A[r*widthA + kk + tidx] : 0.0;
       }
-        
+
 
       /***************************************
        * Subtask 3.1.2:
-       * Please insert here the code that collectively copies 
+       * Please insert here the code that collectively copies
        *    with the Ty x Tx threads of the Cuda block the
-       *    following slice of B: 
-       *         B[kk : kk+Tk][jjj : jjj + Tx*Rx] 
-       *    to the shared-memory buffer Bloc, such that all accesses 
+       *    following slice of B:
+       *         B[kk : kk+Tk][jjj : jjj + Tx*Rx]
+       *    to the shared-memory buffer Bloc, such that all accesses
        *    to B (and Bloc) are coalesced, i.e., consecutive
        *    threads on the threadIdx.x dimension having the same
        *    threadIdx.y value read consecutive locations in memory!
        * If the index is out of the bounds of B then write 0 in Bloc.
-       * 
+       *
        * Hints:
        * 1. Remember that threadIdx.x = 0 ... Tx-1 and, for simplicity
        *      assume that Ty = Tk, hence threadIdx.y = 0 ... Tk-1
@@ -130,7 +130,7 @@ __global__ void mmmSymBlkRegInnSeqKer(ElTp* A, ElTp* B, ElTp* C, int heightA, in
        *      widthB and the column index is less than widthA
        * 3. Remember that B is a flat one-dimensional array, hence you
        *      need to flatten the indexing, but
-       *      Bloc is a two-dimensional array 
+       *      Bloc is a two-dimensional array
        *      (see definitiona at the begining of kernels).
        **************************************************************/
       Bloc[tidy][j - jjj] = (j < widthB && kk + tidy < widthA) ?
@@ -147,18 +147,18 @@ __global__ void mmmSymBlkRegInnSeqKer(ElTp* A, ElTp* B, ElTp* C, int heightA, in
                 /***************************************
                  * Cuda Exercise 3:
                  * Task 3.2:
-                 * 1. Get rid of the "if" and then: 
+                 * 1. Get rid of the "if" and then:
                  * 2. Please modify the "css[i][j] +=" statement
                  *    below to refer to Aloc and Bloc instead
                  *    of A and B.
-                 * This assumes of course that you have 
+                 * This assumes of course that you have
                  *   already solved Task 3.1.
                  ***************************************/
                   if( (iii + threadIdx.y*Ry + i < heightA) &&
                       (kk+k < widthA) &&
                       (jjj + threadIdx.x*Rx + j < widthB)
                     )
-                  css[i][j] +=  
+                  css[i][j] +=
                     A[ (iii + threadIdx.y*Ry + i)*widthA + (kk + k)] *
                     B[ (kk+k)*widthB + jjj + threadIdx.x*Rx + j] ;
               }
