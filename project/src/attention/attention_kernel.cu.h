417f8165e4785a98d136779182aa44916eea3ef8
Cédric Laubacher <cedric@laubacher.io>
2025-10-14 13:14:35 +0200

    Implement softmax kernels: naive, online, and shared_mem

--- project/src/attention/attention_kernel.cu.h
+++ project/src/attention/attention_kernel.cu.h
@@ -1,20 +1,18 @@
-
-
 /*
-Some of the code in this file is inspired by the lecture notes on how to make 
-tiled Matrix Multiplication, chapter 6.3. 
+Some of the code in this file is inspired by the lecture notes on how to make
+tiled Matrix Multiplication, chapter 6.3.
 */
 
-
+#include <cmath>
 template<class ElTp, int T>
 __global__ void compute_S(ElTp* Q, ElTp* K_tr, int N, int d, ElTp* S) {
-    
+
     __shared__ ElTp Q_block[T][T];
     __shared__ ElTp K_tr_block[T][T];
-    
+
     int row = blockIdx.y * T + threadIdx.y;
     int col = blockIdx.x * T + threadIdx.x;
-    
+
     ElTp s = 0.0;
     for (int kk = 0; kk < d; kk += T) {
         // load Q and K^T by blocks from global memory
@@ -34,31 +32,145 @@ __global__ void compute_S(ElTp* Q, ElTp* K_tr, int N, int d, ElTp* S) {
         __syncthreads();
     }
 
-    // write S to global memory 
+    // write S to global memory
     if (row < N && col < N)
         S[row*N+ col] = s;
 }
 
+// naive kernel
+// non-coalesced access to global memory
+template<class ElTp>
+__global__ void compute_P_naive(ElTp* S, ElTp* P, int N) {
+    int row = blockDim.x*blockIdx.x + threadIdx.x;
 
+    if (row<N) {
+        ElTp max_el = -INFINITY;
+        ElTp norm = 0;
+
+        //calculate max_el for better numerical stability
+        for (int i=0; i<N; i++){
+            max_el = max(max_el, S[row*N + i]);
+        }
+
+        //calculate norm
+        for (int i=0; i<N; i++){
+            norm += exp(S[row*N + i] - max_el);
+        }
+
+        //write to P
+        for (int i=0; i<N; i++){
+            P[row*N + i] = exp(S[row*N + i] - max_el)/norm;
+        }
+    }
+}
+
+template<class ElTp>
+__global__ void compute_P_online(ElTp* S, ElTp* P, int N) {
+    int row = blockDim.x*blockIdx.x + threadIdx.x;
+
+    if (row<N) {
+        ElTp max_el = -INFINITY;
+        ElTp norm = 0;
+
+        //single pass for max computation and norm
+        for (int i=0; i<N; i++){
+            ElTp curr_el = S[row*N + i];
+
+            if (curr_el>max_el){
+                norm *= exp(max_el - curr_el);
+                max_el = curr_el;
+            }
+            norm += exp(curr_el - max_el);
+        }
+
+        //write to P
+        for (int i=0; i<N; i++){
+            P[row*N + i] = exp(S[row*N + i] - max_el)/norm;
+        }
+    }
+}
+
+
+// this kernel makes use of shared memory and reductions
+// it also ensures coalesced memory access
+// each block computes one row
 template<class ElTp, int T>
-__global__ void compute_P(ElTp* S, int N) {
-    // read S from global memory TODO
+__global__ void compute_P_shared_mem(ElTp* S, ElTp* P, int N) {
 
-    // compute P = softmax(S) TODO
+    // shared memory
+    // TODO: make it more flexible
+    __shared__ ElTp scratch[1024];
 
-    // write P to global memory TODO
+    int row = blockIdx.x;
+    int tid = threadIdx.x;
+
+    if (row>=N) return;
+
+    ElTp thread_max = -INFINITY;
+    ElTp thread_norm = 0;
+
+    #pragma unroll
+    for (int i=tid; i<N; i+=blockDim.x){
+        // access in coalesced fashion
+        ElTp el = S[row*N + i];
+
+        if (el>thread_max){
+            thread_norm *= exp(thread_max - el);
+            thread_max = el;
+        }
+
+        thread_norm += exp(el - thread_max);
+    }
+
+    scratch[tid] = thread_max;
+    // make sure all threads on block have computed their norm and max
+    __syncthreads();
+
+    // reduce max over block
+    // example with blockDim.x=1024
+    // tid=0 will first compute max of tid=0 and tid=512 and write it into scratch[0]
+    // then it'll combine its result with the one from tid=256, so max of (0, 256, 768, 1024)
+    for (int s=blockDim.x/2; s>0; s>>=1){
+        if (tid < s){
+            scratch[tid] = max(scratch[tid], scratch[tid + s]);
+        }
+        __syncthreads();
+    }
+    // the block-global has been reduced in tid=0
+    ElTp row_max = scratch[0];
+    __syncthreads();
+
+    // write local norm to scratch
+    scratch[tid] = thread_norm * exp(thread_norm - row_max);
+    __syncthreads();
+
+    // reduce norm over threads
+    for (int s=blockDim.x/2; s>0; s>>=1){
+        if (tid < s){
+            scratch[tid] += scratch[tid + s];
+        }
+        __syncthreads();
+    }
+    // the block-global has been reduced in tid=0
+    ElTp row_norm = scratch[0];
+    __syncthreads();
+
+    #pragma unroll
+    for (int i=tid; i<N; i+=blockDim.x){
+        P[row*N + i] = exp(S[row*N + i] - row_max)/row_norm;
+    }
 }
 
 
 template<class ElTp, int T>
 __global__ void compute_O(ElTp* V, ElTp* P, int N, int d, ElTp* O) {
-    
+
     __shared__ ElTp P_block[T][T];
     __shared__ ElTp V_block[T][T];
-    
+
     int row = blockIdx.y * T + threadIdx.y;
     int col = blockIdx.x * T + threadIdx.x;
-        
+
     ElTp o = 0.0;
     for (int kk = 0; kk < d; kk += T) {
         // load P and V by blocks from global memory
@@ -69,7 +181,7 @@ __global__ void compute_O(ElTp* V, ElTp* P, int N, int d, ElTp* O) {
 
         __syncthreads();
 
-        // compute O = P * V 
+        // compute O = P * V
         #pragma unroll
         for (int k = 0; k < T; k++) {
             o += P_block[threadIdx.y][k] * V_block[k][threadIdx.x];
@@ -82,6 +194,3 @@ __global__ void compute_O(ElTp* V, ElTp* P, int N, int d, ElTp* O) {
     if (row < N && col < d)
         O[row*d + col] = o;
 }
-
-
-
